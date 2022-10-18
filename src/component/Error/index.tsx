8a4c646fb9dbe70b456f8339c2007ac52c19c7f2
Irin <iraantonova1998@list.ru>
2022-10-18 20:29:45 +0300

    return btn

--- src/component/Error/index.tsx
+++ src/component/Error/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as NotFound } from 'assets/img/404.svg';
 import styles from './index.module.scss';
 
@@ -7,13 +8,21 @@ interface IProps {
 }
 
 const Error: React.FC<IProps> = ({ text }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapper_text}>
         <NotFound />
         <p>{text}</p>
       </div>
-      <button type="button">Return</button>
+      <button type="button" onClick={handleClick}>
+        Return
+      </button>
     </div>
   );
 };
